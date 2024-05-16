2804e11e66b78613354506a35fd5c6276fd676fc
yangyang <yangjoanne216@gmail.com>
2024-05-16 15:20:21 +0200

    session 2 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PostListComponent } from './post-list/post-list.component';
+import { NgModel } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar.component';
+
+const routes: Routes = [
+  {path:'',component:PostListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PostService } from './services/post.service';
+import { PostListComponent } from './post-list/post-list.component';
+import { CommonModule } from '@angular/common';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    PostListComponent,
+    PostListItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+  providers: [PostService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
