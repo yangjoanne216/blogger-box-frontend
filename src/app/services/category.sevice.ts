b1fc0cc601353b6bb7432015caac122979d14c06
yangyang <yangjoanne216@gmail.com>
2024-06-01 21:09:17 +0200

    Implementation of the CategoryService.ts

--- src/app/services/category.sevice.ts
+++ src/app/services/category.sevice.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Category } from '../data/category';
 
 @Injectable()
@@ -10,24 +10,36 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   //Making HTTP GET Resquests: get all Categories
-
   getAll(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.categoriesUrl);
+    return this.http
+      .get<Category[]>(this.categoriesUrl)
+      .pipe(catchError(this.handleError<Category[]>('getAll')));
+  }
+
+  //Making HTTP GET Resquests: get categorie by id
+  getById(id: string): Observable<Category> {
+    return this.http
+      .get<Category>(`${this.categoriesUrl}/${id}`)
+      .pipe(catchError(this.handleError<Category>('getById')));
   }
 
   //Making HTTP POST Requests:
-  /*create(category:CategoryCreateInput):Observable<Category>{
-    return this.http.post<Category>(this.categoriesUrl,category);
-  }*/
+  create(category: Category): Observable<Category> {
+    return this.http
+      .post<Category>(this.categoriesUrl, category)
+      .pipe(catchError(this.handleError<Category>('create', category)));
+  }
 
   //Making HTTP PUT Requests:
   update(category: Category): Observable<Category> {
-    return this.http.put<Category>(this.categoriesUrl, category);
+    return this.http
+      .put<Category>(this.categoriesUrl, category)
+      .pipe(catchError(this.handleError<Category>('put', category)));
   }
 
   //Making HTTP DELETE Requests
   delete(category: Category): Observable<boolean> {
-    return this.http.delete<boolean>('$(this.categoriesUrl)/${category.id}');
+    return this.http.delete<boolean>('${this.categoriesUrl}/${category.id}');
   }
 
   //Handling Errors
