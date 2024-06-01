ae074022f743878a58bd8421a84010591e0aaf09
yangyang <yangjoanne216@gmail.com>
2024-06-02 00:22:39 +0200

    Implementation of the PostService.ts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, catchError, of } from 'rxjs';
 
 import { POSTS, Post } from '../data/post';
 
@@ -9,9 +9,67 @@ export class PostService {
   private postsUrl = '${environment.apiUrl}v1/posts';
   constructor(private http: HttpClient) {}
 
-  //get all posts
-  getPosts(): Observable<Post[]> {
-    const posts = of(POSTS);
-    return posts;
+  //Retrive all post (if there is a parameter, you can also search a post by title or content)
+  getAll(search?: string): Observable<Post[]> {
+    let params = new HttpParams();
+    if (search) {
+      params = params.set('search', search);
+    }
+    return this.http
+      .get<Post[]>(this.postsUrl, { params })
+      .pipe(catchError(this.handleError<Post[]>('getPosts', [])));
+  }
+
+  //Retrieve all posts for a category(By categoriId)",
+  getAllByCategoryId(categoryId: string): Observable<Post[]> {
+    return this.http
+      .get<Post[]>(`${this.postsUrl}/by-category/${categoryId}`)
+      .pipe(catchError(this.handleError<Post[]>('getAllByCategoryId', [])));
+  }
+
+  //Retrieve a post by id
+  getById(id: string): Observable<Post> {
+    return this.http
+      .get<Post>(`${this.postsUrl}/${id}`)
+      .pipe(catchError(this.handleError<Post>('getByCategoryId')));
+  }
+  //Creat a new post
+  //Creates a new post with the given title, content, and category ID provided in the request body. Returns the created post
+  create(post: Post): Observable<Post> {
+    return this.http
+      .post<Post>(`${this.postsUrl}`, post)
+      .pipe(catchError(this.handleError<Post>('create', post)));
+  }
+  //Update an existing post
+  //Updates the title and content of an existing post identified by its ID. Returns the updated post
+  update(id: string, post: Partial<Post>): Observable<Post> {
+    return this.http
+      .put<Post>(`${this.postsUrl}/${id}`, post)
+      .pipe(catchError(this.handleError<Post>('update', undefined)));
+  }
+  //Delete an existing post
+  //Deletes the post identified by its ID. Returns a boolean indicating whether the deletion was successful
+  deleteById(id: string): Observable<boolean> {
+    return this.http
+      .delete<boolean>(`${this.postsUrl}/${id}`)
+      .pipe(catchError(this.handleError<boolean>('deleteById', false)));
+  }
+  //Retrieve all posts ordered by creation date
+  //Returns all posts sorted by their creation dates in descending order
+  orderedByCreatedDate(): Observable<Post[]> {
+    return this.http
+      .get<Post[]>(`${this.postsUrl}/sorted`)
+      .pipe(
+        catchError(this.handleError<Post[]>('orderedByCreatedDate', undefined))
+      );
+  }
+
+  //Handling Errors
+  protected handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error('${operation} failed ${eror.message}', error);
+      //Let the app keep running by returning an empty result
+      return of(result as T);
+    };
   }
 }
